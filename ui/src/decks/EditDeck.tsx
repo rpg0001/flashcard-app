5cb7c1af8955191187a92f4acc0ce92426a4fa6f
Robyn <77750040+rpg0001@users.noreply.github.com>
2025-03-23 14:39:38 +0000

    (ui) confirm deck visibility changes

--- ui/src/decks/EditDeck.tsx
+++ ui/src/decks/EditDeck.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Deck, DeckVisibility, getDeck, updateDeck } from "../services/decks";
+import { getCardCountString } from "./helpers";
 
 export function EditDeck() {
     const navigate = useNavigate();
@@ -9,6 +10,7 @@ export function EditDeck() {
     const [name, setName] = useState<string>();
     const [description, setDescription] = useState<string>();
     const [visibility, setVisibility] = useState<DeckVisibility>();
+    const [confirmationRequired, setConfirmationRequired] = useState<boolean>(false);
 
     useEffect(() => {
         const fetchDeck = async () => setDeck(await getDeck(Number(id)));
@@ -30,7 +32,11 @@ export function EditDeck() {
 
     function handleSubmit(event: any) {
         event.preventDefault();
-        doUpdateDeck();
+        if (visibility !== deck?.visibility) {
+            setConfirmationRequired(true);
+        } else {
+            doUpdateDeck();
+        }
     }
 
     function selectVisibility(value: string | undefined) {
@@ -38,55 +44,97 @@ export function EditDeck() {
             setVisibility(value as DeckVisibility);
         }
     }
+
+    function cancelUpdate() {
+        setConfirmationRequired(false);
+    }
     
     return (
-        <div>
+        <div className="list-v">
             <Link to={`/decks/${id}`} >{"<"} Back to deck details</Link>
-            <h1>Edit deck #{id}</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='name'>Name</label>
-                    <input 
-                        type='text' 
-                        id='name' 
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor='description'>Description</label>
-                    <input 
-                        type='text' 
-                        id='description' 
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <fieldset>
-                        <legend>Visibility</legend>
+            <h1>Edit deck: {`"${deck?.name ?? "(name unknown)"}"`}</h1>
+            <small>This deck contains {getCardCountString(deck?.cardCount ?? 0)}</small>
+            {confirmationRequired ?
+                <ConfirmEditDeck 
+                    deck={deck} 
+                    newVisibility={visibility} 
+                    doUpdateDeck={doUpdateDeck}
+                    cancelUpdate={cancelUpdate}
+                />
+                :
+                <form onSubmit={handleSubmit} className="list-v">
+                    <div>
+                        <label htmlFor='name'>Name</label>
                         <input 
-                            type='radio' 
-                            id='visibility-private' 
-                            name='visibility-private' 
-                            value={DeckVisibility.PRIVATE}
-                            checked={visibility === DeckVisibility.PRIVATE}
-                            onChange={(e) => selectVisibility(e.target.value)}
+                            type='text' 
+                            id='name' 
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
-                        <label htmlFor="visibility-private">Private</label>
+                    </div>
+                    <div>
+                        <label htmlFor='description'>Description</label>
                         <input 
-                            type='radio' 
-                            id='visibility-public' 
-                            name='visibility-public' 
-                            value={DeckVisibility.PUBLIC}
-                            checked={visibility === DeckVisibility.PUBLIC}
-                            onChange={(e) => selectVisibility(e.target.value)}
+                            type='text' 
+                            id='description' 
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
                         />
-                        <label htmlFor="visibility-public">Public</label>
-                    </fieldset>
-                </div>
-                <button type='submit'>Update</button>
-            </form>
+                    </div>
+                    <div>
+                        <fieldset>
+                            <legend>Visibility</legend>
+                            <input 
+                                type='radio' 
+                                id='visibility-private' 
+                                name='visibility-private' 
+                                value={DeckVisibility.PRIVATE}
+                                checked={visibility === DeckVisibility.PRIVATE}
+                                onChange={(e) => selectVisibility(e.target.value)}
+                            />
+                            <label htmlFor="visibility-private">Private</label>
+                            <input 
+                                type='radio' 
+                                id='visibility-public' 
+                                name='visibility-public' 
+                                value={DeckVisibility.PUBLIC}
+                                checked={visibility === DeckVisibility.PUBLIC}
+                                onChange={(e) => selectVisibility(e.target.value)}
+                            />
+                            <label htmlFor="visibility-public">Public</label>
+                        </fieldset>
+                    </div>
+                    <div>
+                        <button type='submit'>Update</button>
+                    </div>
+                </form>
+            }
         </div>
     )
+}
+
+function ConfirmEditDeck(props: {
+    deck: Deck | undefined,
+    newVisibility: DeckVisibility | undefined,
+    doUpdateDeck: () => Promise<void>,
+    cancelUpdate: () => void
+}) {
+    if (!props.deck || !props.newVisibility) {
+        return (
+            <p>An unexpected error occurred</p>
+        )
+    } else {
+        return (
+            <div className="list-v">
+                <p>
+                    Are you sure you want to submit this update? This would change the deck's
+                    visibility from <strong>{props.deck.visibility}</strong> to <strong>{props.newVisibility}</strong>.
+                </p>
+                <div className="list-h">
+                    <button onClick={props.doUpdateDeck}>Confirm update</button>
+                    <button onClick={props.cancelUpdate}>Cancel</button>
+                </div>
+            </div>
+        )
+    }
 }
